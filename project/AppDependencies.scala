251726bc2f4ea0c7acdda0b5b48b6e163f89bf88
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2024-02-05 14:21:59 +0000

    APIS-6764: play 3.0 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,21 +5,21 @@ object AppDependencies {
 
   lazy val libraryDependencies = compile ++ test
 
-  private lazy val bootstrapVersion = "7.12.0"
+  private lazy val bootstrapVersion = "8.4.0"
   private lazy val hmrcMongoVersion = "1.7.0"
   private lazy val scalaJVersion    = "2.4.2"
   val commonDomainVersion           = "0.10.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "api-platform-common-domain" % commonDomainVersion
   )
 
   private lazy val test = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28"         % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"         % hmrcMongoVersion,
     "org.scalaj"        %% "scalaj-http"                     % scalaJVersion,
-    "uk.gov.hmrc"       %% "bootstrap-test-play-28"          % bootstrapVersion,
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"          % bootstrapVersion,
     "org.mockito"       %% "mockito-scala-scalatest"         % "1.17.29",
     "uk.gov.hmrc"       %% "api-platform-test-common-domain" % commonDomainVersion
   ).map(_ % "test, it")
