632504ccd541a6ff32e902282cdd65d017869d1f
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2026-01-13 08:18:18 +0000

    API-8888 - Updating bootstrap version to fix security vuln

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   lazy val libraryDependencies = compile ++ test
 
-  private lazy val bootstrapVersion = "9.12.0"
+  private lazy val bootstrapVersion = "10.5.0"
   private lazy val hmrcMongoVersion = "2.6.0"
   val commonDomainVersion           = "0.18.0"
 
